9d97868c4cb28f4fb465e68d1858182047afdde8
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2018-01-21 19:21:33 -0800

    Track forward list.

---
+++ forward_list.h
@@ -0,0 +1,11 @@
+//  :copyright: (c) 2017-2018 Alex Huszagh.
+//  :license: MIT, see licenses/mit.md for more details.
+/**
+ *  \addtogroup PyCPP
+ *  \brief STL forward_list with allocator erasure from iterators.
+ *
+ */
+
+#pragma once
+
+#include <pycpp/stl/container/forward_list.h>
